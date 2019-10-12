174226327acf207a8a34916f634bfcfd0e85281b
SandeepVarma97 <56278231+SandeepVarma97@users.noreply.github.com>
2019-10-12 15:49:50 -0700

    Update chuck.py

--- chuck.py
+++ chuck.py
@@ -5,9 +5,11 @@ import Blog
 
 blog = Blog.Blog()
 
-# t1 = blog._rd(("bob","distsys",str))
+t1 = blog._rd(("bob","distsys",str))
+print(t1)
 
 t2 = blog._rd(("alice","gtcn",str))
 print(t2)
 
-# t3 = blog._rd(("bob","gtcn",str))
+t3 = blog._rd(("bob","gtcn",str))
+print(t3)
