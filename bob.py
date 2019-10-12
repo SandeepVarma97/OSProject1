1ff2032b45b0267e4f8294603f54fcbca3e93cb9
SandeepVarma97 <56278231+SandeepVarma97@users.noreply.github.com>
2019-10-12 15:48:29 -0700

    Update bob.py

--- bob.py
+++ bob.py
@@ -5,6 +5,6 @@ import Blog
 
 blog = Blog.Blog()
 
-# blog._out(("bob","distsys","I am studying chap 2"))
-# blog._out(("bob","distsys","The linda example’s pretty simple"))
-# blog._out(("bob","gtcn","Cool book!"))
+print(blog._out(("bob","distsys","I am studying chap 2")))
+print(blog._out(("bob","distsys","The linda example’s pretty simple")))
+print(blog._out(("bob","gtcn","Cool book!")))
