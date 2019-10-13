295571efa541c0d12a7aa62dbf31c6c03771c596
SandeepVarma97 <56278231+SandeepVarma97@users.noreply.github.com>
2019-10-13 14:07:18 -0700

    Update bob.rb

--- bob.rb
+++ bob.rb
@@ -20,6 +20,6 @@ poster = "bob"
 
 blog = Blog.new()
 
-# blog._out(("bob","distsys","I am studying chap 2"))
-# blog._out(("bob","distsys","The linda example’s pretty simple"))
-# blog._out(("bob","gtcn","Cool book!"))
+puts(blog._out(("bob","distsys","I am studying chap 2")))
+puts(blog._out(("bob","distsys","The linda example’s pretty simple")))
+puts(blog._out(("bob","gtcn","Cool book!")))
